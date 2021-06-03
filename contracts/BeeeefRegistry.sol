818c3c3a6e5828e114f0cdb6b8afdbb9840544de
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2021-06-03 19:27:24 +1000

    WIP

--- contracts/BeeeefRegistry.sol
+++ contracts/BeeeefRegistry.sol
@@ -3,17 +3,40 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
+// ----------------------------------------------------------------------------
+// Curated contract
+// ----------------------------------------------------------------------------
+contract Curated {
+    address public curator;
+
+    event CuratorTransferred(address indexed _from, address indexed _to);
+
+    modifier onlyCurator {
+        require(msg.sender == curator);
+        _;
+    }
+
+    constructor() {
+        curator = msg.sender;
+    }
+    function transferCurator(address _newCurator) public onlyCurator {
+        emit CuratorTransferred(curator, _newCurator);
+        curator = _newCurator;
+    }
+}
+
 
 // ----------------------------------------------------------------------------
 // Entries Data Structure
 // ----------------------------------------------------------------------------
 library Entries {
     struct Entry {
-        uint timestamp;
         uint index;
+        uint64 timestamp;
         address account;
         address token;
-        uint permission;
+        uint32 permission;
+        uint32 curatorOption;
     }
     struct Data {
         bool initialised;
@@ -23,7 +46,8 @@ library Entries {
 
     event EntryAdded(bytes32 key, address account, address token, uint permission, uint totalAfter);
     event EntryRemoved(bytes32 key, address account, address token, uint totalAfter);
-    event EntryUpdated(uint256 indexed tokenId, address account, address token, uint permission);
+    event EntryUpdated(bytes32 key, address account, address token, uint permission);
+    event EntryCurated(bytes32 key, address account, address token, uint curatorOption);
 
     function init(Data storage self) internal {
         require(!self.initialised);
@@ -35,17 +59,19 @@ library Entries {
     function hasKey(Data storage self, bytes32 key) internal view returns (bool) {
         return self.entries[key].timestamp > 0;
     }
-    function add(Data storage self, address account, address token, uint permission) internal {
+    function add(Data storage self, address account, address token, uint32 permission) internal {
         bytes32 key = generateKey(account, token);
-        console.log("Entries.add account: %s, token: %s, permission: %s", account, token, permission);
+        // console.log("Entries.add account: %s, token: %s, permission: %s", account, token, permission);
         require(self.entries[key].timestamp == 0);
         self.index.push(key);
-        self.entries[key] = Entry(block.timestamp, self.index.length - 1, account, token, permission);
+        self.entries[key] = Entry(self.index.length - 1, uint64(block.timestamp), account, token, permission, 0);
         emit EntryAdded(key, account, token, permission, self.index.length);
     }
     function remove(Data storage self, address account, address token) internal {
         bytes32 key = generateKey(account, token);
         require(self.entries[key].timestamp > 0);
+        // Not required? Hash should mismatch
+        // require(self.entries[key].account == msg.sender);
         uint removeIndex = self.entries[key].index;
         emit EntryRemoved(key, account, token, self.index.length - 1);
         uint lastIndex = self.index.length - 1;
@@ -57,42 +83,45 @@ library Entries {
             self.index.pop();
         }
     }
-
-    // function setValue(Data storage self, uint256 tokenId, string memory key, string memory value) internal {
-    //     Value storage _value = self.entries[key];
-    //     require(_value.timestamp > 0);
-    //     _value.timestamp = block.timestamp;
-    //     emit AttributeUpdated(tokenId, key, value);
-    //     _value.value = value;
-    // }
+    function update(Data storage self, address account, address token, uint32 permission) internal {
+        bytes32 key = generateKey(account, token);
+        Entry storage entry = self.entries[key];
+        require(entry.timestamp > 0);
+        entry.timestamp = uint64(block.timestamp);
+        entry.permission = permission;
+        emit EntryUpdated(key, account, token, permission);
+    }
+    function curate(Data storage self, address account, address token, uint32 curatorOption) internal {
+        bytes32 key = generateKey(account, token);
+        Entry storage entry = self.entries[key];
+        require(entry.timestamp > 0);
+        entry.curatorOption = curatorOption;
+        emit EntryCurated(key, account, token, curatorOption);
+    }
     function length(Data storage self) internal view returns (uint) {
         return self.index.length;
     }
 }
 
 
-contract BeeeefRegistry {
+contract BeeeefRegistry is Curated {
 
     using Entries for Entries.Data;
     using Entries for Entries.Entry;
 
-    // mapping(address => Entries.Data) private entries;
     Entries.Data private entries;
-
-    string greeting;
 
     event EntryAdded(bytes32 key, address account, address token, uint permission, uint totalAfter);
     event EntryRemoved(bytes32 key, address account, address token, uint totalAfter);
-    event EntryUpdated(uint256 indexed tokenId, address account, address token, uint permission);
+    event EntryUpdated(bytes32 key, address account, address token, uint permission);
+    event EntryCurated(bytes32 key, address account, address token, uint curatorOption);
 
-    constructor(string memory _greeting) {
+    constructor() {
         entries.init();
-        console.log("Deploying a BeeeefRegistry with greeting:", _greeting);
-        greeting = _greeting;
     }
 
-    function addEntry(address token, uint permission) public {
-        console.log("addEntry token: %s, permission: %s", token, permission);
+    function addEntry(address token, uint32 permission) public {
+        // console.log("addEntry token: %s, permission: %s", token, permission);
         require(token != address(0), "Token cannot be null");
         entries.add(msg.sender, token, permission);
     }
@@ -100,6 +129,15 @@ contract BeeeefRegistry {
     function removeEntry(address token) public {
         require(token != address(0), "Token cannot be null");
         entries.remove(msg.sender, token);
+    }
+    function updateEntry(address token, uint32 permission) public {
+        // console.log("addEntry token: %s, permission: %s", token, permission);
+        require(token != address(0), "Token cannot be null");
+        entries.update(msg.sender, token, permission);
+    }
+
+    function curateEntry(address account, address token, uint32 curatorOption) public onlyCurator {
+        entries.curate(account, token, curatorOption);
     }
 
     function getEntryByIndex(uint i) public view returns (address _account, address _token, uint _permission) {
@@ -112,12 +150,18 @@ contract BeeeefRegistry {
         return entries.length();
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
-    }
-
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function getEntries() public view returns (address[] memory accounts, address[] memory tokens, uint32[] memory permissions, uint32[] memory curatorOptions) {
+        uint length = entries.length();
+        accounts = new address[](length);
+        tokens = new address[](length);
+        permissions = new uint32[](length);
+        curatorOptions = new uint32[](length);
+        for (uint i = 0; i < length; i++) {
+            Entries.Entry memory entry = entries.entries[entries.index[i]];
+            accounts[i] = entry.account;
+            tokens[i] = entry.token;
+            permissions[i] = entry.permission;
+            curatorOptions[i] = entry.curatorOption;
+        }
     }
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -6,37 +6,46 @@ var TESTACCOUNT2 = "0x0000000000000000000000000000000000022222";
 describe("BeeeefRegistry", function() {
 
   async function printEntries(contract) {
-    const length = await contract.entriesLength();
-    console.log("printEntries length: " + length);
-    for (let i = 0; i < length; i++) {
-      const entry = await contract.getEntryByIndex(i);
-      console.log("printEntries " + i + ": " + JSON.stringify(entry));
-    }
+    const entries = await contract.getEntries();
+    console.log("printEntries entries: " + JSON.stringify(entries));
+
+    // const length = await contract.entriesLength();
+    // console.log("printEntries length: " + length);
+    // for (let i = 0; i < length; i++) {
+    //   const entry = await contract.getEntryByIndex(i);
+    //   console.log("printEntries " + i + ": " + JSON.stringify(entry));
+    // }
   }
 
   it("Should return the new greeting once it's changed", async function() {
 
     let [ownerSigner, user0Signer] = await ethers.getSigners();
 
-    const Greeter = await ethers.getContractFactory("BeeeefRegistry");
-    const greeter = await Greeter.deploy("Hello, world!");
+    const BeeeefRegistry = await ethers.getContractFactory("BeeeefRegistry");
+    const registry = await BeeeefRegistry.deploy();
 
-    await greeter.deployed();
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    await registry.deployed();
+    // expect(await registry.greet()).to.equal("Hello, world!");
 
-    const addEntryTx0 = await greeter.addEntry(TESTACCOUNT1, 1);
-    await printEntries(greeter);
+    const curator = await registry.curator();
+    console.log("curator: " + curator);
 
-    const addEntryTx1 = await greeter.addEntry(TESTACCOUNT2, 1);
-    await printEntries(greeter);
+    const addEntryTx0 = await registry.connect(user0Signer).addEntry(TESTACCOUNT1, 1);
+    await printEntries(registry);
 
-    const removeEntryTx0 = await greeter.removeEntry(TESTACCOUNT1);
-    await printEntries(greeter);
+    const addEntryTx1 = await registry.connect(user0Signer).addEntry(TESTACCOUNT2, 1);
+    await printEntries(registry);
 
-    const removeEntryTx1 = await greeter.removeEntry(TESTACCOUNT2);
-    await printEntries(greeter);
+    const updateEntryTx0 = await registry.connect(user0Signer).updateEntry(TESTACCOUNT2, 2);
+    await printEntries(registry);
 
-    await greeter.setGreeting("Hola, mundo!");
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    const curateEntryTx0 = await registry.connect(ownerSigner).curateEntry(user0Signer.address, TESTACCOUNT2, 33);
+    await printEntries(registry);
+
+    const removeEntryTx0 = await registry.connect(user0Signer).removeEntry(TESTACCOUNT1);
+    await printEntries(registry);
+
+    const removeEntryTx1 = await registry.connect(user0Signer).removeEntry(TESTACCOUNT2);
+    await printEntries(registry);
   });
 });
